3ef97f7391e6ad963a484609936946df16b4a7a1
Mark Lubin <melubin@gmail.com>
2012-11-17 22:45:11 -0500

    removed no longer needed files

--- extraction/csvmake.py
+++
@@ -1,9 +0,0 @@
-f = open('flickrdataV2FINAL.txt','r')
-out = open('platlong.csv','w')
-out.write("lat,long\n")
-for line in f.readlines():
-	fields = line.split(',')
-	lat = fields[2].split(" ")[3]
-	lon = fields[3].split(" ")[3]
-	out.write("{:s},{:s}".format(lat,lon))
-out.close()
